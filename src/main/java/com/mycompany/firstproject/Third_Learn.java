91877dbc34d774893e48f5e0f210e6343523da78
User <ameerjassr2006@gmail.com>
2025-10-30 09:55:28 +0800

    Second && Third_learn

---
+++ src/main/java/com/mycompany/firstproject/Third_Learn.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.firstproject;
+
+/**
+ *
+ * @author User
+ */
+public class Third_Learn {
+    
+    public static void main(String[] args){
+        
+        byte age = 30;
+        long viewsCount = 358_624_555_988L;
+        System.out.println(viewsCount);
+        
+        float price = 10.5F;
+        char letter = 'A';
+        System.out.println(price);
+        
+        boolean isEligible = false;
+        System.out.println(isEligible);
+    }
+    
+}
