e0e099b9942c1b4b20bea51406d20e00c9a2109a
User <ameerjassr2006@gmail.com>
2025-11-11 10:27:50 +0800

    Amir

---
+++ src/main/java/com/mycompany/firstproject/Tst0/T01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.firstproject.Tst0;
+
+/**
+ *
+ * @author User
+ */
+public class T01 {
+    public static void main(String[] args) {
+        for (int i =1; i<20; i++){
+            System.out.println(++i);
+        }
+            
+    }
+    
+}
