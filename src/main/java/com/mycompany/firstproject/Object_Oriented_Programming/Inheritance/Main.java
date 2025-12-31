64824a8d024dc1a15ba01459e06b7d7b16ddbb5b
ameerjasser <ameerjassr2006@gmail.com>
2025-12-31 11:24:03 +0800

    The Object Class

--- src/main/java/com/mycompany/firstproject/Object_Oriented_Programming/Inheritance/Main.java
+++ src/main/java/com/mycompany/firstproject/Object_Oriented_Programming/Inheritance/Main.java
@@ -2,9 +2,9 @@ package com.mycompany.firstproject.Object_Oriented_Programming.Inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        var control = new TextBox();
-        control.disable();
-        System.out.println(control.isEnabled());
+        var box1 = new TextBox();
+        System.out.println(box1.toString());
+        System.out.println(box1.hashCode()); // this will print a number, which calculated based on the address of this object in the memory
     }
 }
         
