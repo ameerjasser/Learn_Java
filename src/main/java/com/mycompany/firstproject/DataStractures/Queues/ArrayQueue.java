ab615979e125c5c76719d0c02d637e5aa9c9318b
ameerjasser <ameerjassr2006@gmail.com>
2026-05-01 19:14:23 +0800

    Building a Queue using an Array

--- src/main/java/com/mycompany/firstproject/DataStractures/Queues/ArrayQueue.java
+++ src/main/java/com/mycompany/firstproject/DataStractures/Queues/ArrayQueue.java
@@ -3,23 +3,34 @@ package com.mycompany.firstproject.DataStractures.Queues;
 import java.util.Arrays;
 
 public class ArrayQueue {
-    private int[] array = new int[4];
+    private int[] array;
     private int first = 0;
     private int last = 0;
 
+    public ArrayQueue(int capacity) {
+        array = new int[capacity];
+    }
     public void enqueue(int item){
-        if(isFull()) throw new IllegalArgumentException();
+        if(isFull()) throw new IllegalArgumentException("It's full!!!");
 
-        //first == 0
-        for(int i=last; i>=0; i++){
-            array[i+1] = array[i];
-        }
-        array[0] = item;
-        last++;
+        array[last++] = item;
     }
 
-    public String printQueue(){
-        return Arrays.toString(array);
+    public int dequeue(){
+        if(isEmpty()) throw new IllegalArgumentException("It's empty!!!");
+        var item = array[first];
+        array[first++] =0;
+        return item;
+    }
+    public int peek(){
+        return array[first];
+    }
+    @Override
+    public String toString(){
+        return Arrays.toString(Arrays.copyOfRange(array, first, last));
+    }
+    private boolean isEmpty() {
+        return last==0;
     }
     private boolean isFull() {
         return last == array.length;
--- src/main/java/com/mycompany/firstproject/DataStractures/Queues/Main.java
+++ src/main/java/com/mycompany/firstproject/DataStractures/Queues/Main.java
@@ -1,7 +1,18 @@
 package com.mycompany.firstproject.DataStractures.Queues;
 
-//import com.mycompany.firstproject.Object_Oriented_Programming.Inheritance.topic2.main;
-
 public class Main {
- main    
+    public static void main(String[] args) {
+        ArrayQueue queue = new ArrayQueue(6);
+
+        queue.enqueue(20);
+        queue.enqueue(15);
+        queue.enqueue(10);
+        System.out.println(queue);
+        queue.dequeue();
+        queue.enqueue(5);
+        queue.enqueue(66);
+        System.out.println(queue);
+        System.out.println(queue.dequeue());
+
+    }
 }
